6731b723bdf5cd0f3f25d0a5fecb777e14b82947
Arthur <arthurmatike@gmail.com>
2026-09-19 18:19:38 +0100

    added links to the icons

--- js/5-projects-creation.js
+++ js/5-projects-creation.js
@@ -14,26 +14,28 @@ let projectList = '';
 let arrayOfProjects = [];
 
 arrayOfProjects.push(
-  {title:'Programming - C', desc: 'Created a Stack, Queue, Circular Queue in C', img: '../images-and-icons/icons/github-icon-svgrepo-com.svg'
+  {title:'Programming - C', desc: 'Created a Stack, Queue, Circular Queue in C', img: '../images-and-icons/icons/github-icon-svgrepo-com.svg', repo_link: 'https://github.com/karma7070/Very-simple-C-tasks-I-completed'
 })
 
 arrayOfProjects.push(
-  {title:'Programming - C++', desc: 'Built a Secure Messaging System with XOR cipher and OpenSSL', img: '../images-and-icons/icons/github-icon-svgrepo-com.svg'})
+  {title:'Programming - C++', desc: 'Built a Secure Messaging System with XOR cipher and OpenSSL', img: '../images-and-icons/icons/github-icon-svgrepo-com.svg', repo_link: 'https://github.com/karma7070/Secure-messaging-system-in-C---with-openSSL-and-XOR-cipher'
+
+  })
 
 arrayOfProjects.push({
-  title:'Backend Development (PostgreSQL + Springboot +...)', desc: 'Built the products and damaged modules in the employee system of the company I interned at (Kings Digital)', img:'../images-and-icons/icons/github-icon-svgrepo-com.svg'
+  title:'Backend Development (PostgreSQL + Springboot +...)', desc: 'Built the products and damaged modules in the employee system of the company I interned at (Kings Digital)', img:'../images-and-icons/icons/github-icon-svgrepo-com.svg', repo_link: ''
 })  
 
 arrayOfProjects.push({
-  title: 'FullStack Dev(Springboot) - My first java project: Tourist booking system', desc:'Built a system for booking travels to touristic sites with group members from school(School project)', img:'../images-and-icons/icons/github-icon-svgrepo-com.svg'
+  title: 'FullStack Dev(Springboot) - My first java project: Tourist booking system', desc:'Built a system for booking travels to touristic sites with group members from school(School project)', img:'../images-and-icons/icons/github-icon-svgrepo-com.svg', repo_link: 'https://github.com/karma7070/OOP-group-13---Tourism-booking-system'
 })
 
 arrayOfProjects.push({
-  title:'Frontend Development(My Portfolio)', desc: 'Built this very portfolio from scratch', img:'../images-and-icons/icons/github-icon-svgrepo-com.svg'
+  title:'Frontend Development(My Portfolio)', desc: 'Built this very portfolio from scratch', img:'../images-and-icons/icons/github-icon-svgrepo-com.svg', repo_link: 'https://github.com/karma7070/My-Portfolio'
 })
 
 arrayOfProjects.push({
-  title:'Backend Development(Springboot + PostgreSQL)', desc:'Building an app for finding a job in Cameroon or anywhere really with features like a whatsapp API that make communication easy', img:'../images-and-icons/icons/github-icon-svgrepo-com.svg'
+  title:'Backend Development(Springboot + PostgreSQL)', desc:'Building an app for finding a job in Cameroon or anywhere really with features like a whatsapp API that make communication easy', img:'../images-and-icons/icons/github-icon-svgrepo-com.svg', repo_link: 'https://github.com/karma7070/Practice-Backend-for-Job-Finder-Application-with-Springboot-Spring-Modulith-Architecture-'
 })
 
 
@@ -55,6 +57,7 @@ arrayOfProjects.forEach(function(elt){
 
                 </div>
 
+                <a href="${proj.repo_link}">
                 <div class="btn-con">
                   <button>
                     <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
@@ -63,6 +66,7 @@ arrayOfProjects.forEach(function(elt){
                   </svg>
                   </button>
                 </div>
+                </a>
 
             </div>`;
 
